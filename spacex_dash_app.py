dcceba3115ae6234bcc184a155170a9e33a5185c
Alex <paverama@gmail.com>
2022-05-20 06:45:04 -0300

    commit

--- spacex_dash_app.py
+++ spacex_dash_app.py
@@ -80,6 +80,7 @@ def get_pie_chart(entered_site):
         fig=px.pie(filtered_df,values='class count',names='class',title=f"Total Success Launches for site {entered_site}")
         return fig
 
+
 # TASK 4:
 # Add a callback function for `site-dropdown` and `payload-slider` as inputs, `success-payload-scatter-chart` as output
 @app.callback(Output(component_id='success-payload-scatter-chart',component_property='figure'),
